02263738bbeb690e826b4ba766325c38b837c40e
Domingos <dsrm@cin.ufpe.br>
2019-02-04 18:13:38 -0300

    initial commit

---
+++ src/main/java/br/ufpe/cin/imperial/ImperialApplication.java
@@ -0,0 +1,14 @@
+package br.ufpe.cin.imperial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImperialApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/ufpe/cin/imperial/ImperialApplicationTests.java
@@ -0,0 +1,17 @@
+package br.ufpe.cin.imperial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImperialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
